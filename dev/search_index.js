f3c0c0884b4e0c3856a9677e914c99388c5dbbc2
Documenter.jl <documenter@juliadocs.github.io>
2024-08-09 21:24:55 +0000

    build based on 606f08e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PBRTMakie","category":"page"},{"location":"#PBRTMakie","page":"Home","title":"PBRTMakie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PBRTMakie.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PBRTMakie]","category":"page"}]
+}
